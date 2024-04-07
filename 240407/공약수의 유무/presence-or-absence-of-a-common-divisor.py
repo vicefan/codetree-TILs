790ecfbec30bb322a5259efb9bcffbe1f2ab9650
vicefan <131180965+vicefan@users.noreply.github.com>
2024-04-07 19:35:30 +0900

    Title: 공약수의 유무, Time: 110ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240407/공약수의 유무/presence-or-absence-of-a-common-divisor.py
@@ -0,0 +1,12 @@
+arr = list(map(int, input().split()))
+a, b = tuple(arr)
+
+count = 0
+
+for i in range(a, b + 1):
+    if 1920 % i == 0 and 2880 % i == 0:
+        print(1)
+        count += 1
+
+if count == 0:
+    print(0)
