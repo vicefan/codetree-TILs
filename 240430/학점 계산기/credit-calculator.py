b77f300d1f2435407a0219f0b2a2fb946ff77888
vicefan <131180965+vicefan@users.noreply.github.com>
2024-04-30 23:26:51 +0900

    Title: 학점 계산기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240430/학점 계산기/credit-calculator.py
+++ 240430/학점 계산기/credit-calculator.py
@@ -1,5 +1,5 @@
 n = int(input())
-grade = list(map(float, input().split()))
+grade = list(map(float, input().split(" ")))
 av_grade = sum(grade) / n
 
 print(round(av_grade, 1))
@@ -8,7 +8,5 @@ if av_grade >= 4:
     print("Perfect")
 elif av_grade >= 3:
     print("Good")
-elif av_grade < 3 and av_grade >= 0:
-    print(Poor)
 else:
-    pass
+    print("Poor")
