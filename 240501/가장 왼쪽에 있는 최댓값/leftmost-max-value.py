a65796a46e75351d671486e5a4593ac4aa01ad39
vicefan <131180965+vicefan@users.noreply.github.com>
2024-05-01 18:46:17 +0900

    Title: 가장 왼쪽에 있는 최댓값, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240501/가장 왼쪽에 있는 최댓값/leftmost-max-value.py
+++ 240501/가장 왼쪽에 있는 최댓값/leftmost-max-value.py
@@ -3,7 +3,7 @@ arr = list(map(int, input().split()))
 print(arr.index(max(arr)) + 1, end=" ")
 
 while True:
-    if len(arr) == 1:
+    if len(arr) <= 1:
         break
     arr = arr[:arr.index(max(arr))]
     print(arr.index(max(arr)) + 1, end=" ")
