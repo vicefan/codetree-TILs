93d651e8c424164a5a781b9b8e71f297beef2dcd
vicefan <131180965+vicefan@users.noreply.github.com>
2024-05-01 18:43:37 +0900

    Title: 가장 왼쪽에 있는 최댓값, Time: 106ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240501/가장 왼쪽에 있는 최댓값/leftmost-max-value.py
+++ 240501/가장 왼쪽에 있는 최댓값/leftmost-max-value.py
@@ -2,5 +2,6 @@ n = int(input())
 arr = list(map(int, input().split()))
 print(arr.index(max(arr)) + 1, end=" ")
 
-arr = arr[:arr.index(max(arr))]
-print(arr.index(max(arr)) + 1)
+if arr.index(max(arr)) != 0:
+    arr = arr[:arr.index(max(arr))]
+    print(arr.index(max(arr)) + 1)
