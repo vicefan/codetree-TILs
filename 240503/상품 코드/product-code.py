a3f8e8ce48a3987ef6ef3111fa7f7c477eac33f7
vicefan <131180965+vicefan@users.noreply.github.com>
2024-05-03 17:08:10 +0900

    Title: 상품 코드, Time: 239ms, Memory: 24MB, Status: Passed - CodeTree

--- 240503/상품 코드/product-code.py
+++ 240503/상품 코드/product-code.py
@@ -1,16 +1,14 @@
-class Bomb:
-    def __init__(self, code, color, second):
+class Product:
+    def __init__(self, name="codetree", code=50):
+        self.n = name
         self.c = code
-        self.o = color
-        self.s = second
 
 ag = []
 for _ in range(1):
-    x, y, z = input().split()
-    ag.append(Bomb(x, y, int(z)))
+    x, y = input().split()
+    ag.append(Product(x, int(y)))
 
 # ag.sort(key = lambda x: x.?)
 
-print(f"""code : {ag[0].c}
-color : {ag[0].o}
-second : {ag[0].s}""")
+print(f"""product {Product().c} is {Product().n}
+product {ag[0].c} is {ag[0].n}""")
