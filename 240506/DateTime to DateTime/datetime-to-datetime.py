ed9241b9983029ce1e44a17a3f689875750782d9
vicefan <131180965+vicefan@users.noreply.github.com>
2024-05-06 23:19:33 +0900

    Title: DateTime to DateTime, Time: 115ms, Memory: 24MB, Status: Passed - CodeTree

--- 240506/DateTime to DateTime/datetime-to-datetime.py
+++ 240506/DateTime to DateTime/datetime-to-datetime.py
@@ -1,4 +1,4 @@
 d, t, m = map(int, input().split())
 min_calc = (d - 11) * 24 * 60 + (t - 11) * 60 + m - 11
 
-print(min_calc) if min_calc > 0 else print(-1)
+print(min_calc) if min_calc >= 0 else print(-1)
