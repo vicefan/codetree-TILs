7b8928f600ba3b4be9d0437acfa8d24bd15ead5e
vicefan <131180965+vicefan@users.noreply.github.com>
2024-05-07 00:15:45 +0900

    Title: 그 요일은, Time: 108ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240507/그 요일은/the-day-of-the-day.py
+++ 240507/그 요일은/the-day-of-the-day.py
@@ -2,9 +2,9 @@ m1, d1, m2, d2 = map(int, input().split())
 
 num_of_days = [0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
-yo = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
+yo = ["", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
 
-days, cnt, idx = 1, 0, yo.index(input()) + 1
+days, cnt, idx = 1, 0, yo.index(input())
 
 while True:
     if m1 == m2 and d1 == d2 + 1:
