c851ece9dfe3597385288942119c8498de109569
vicefan <131180965+vicefan@users.noreply.github.com>
2024-05-07 13:09:53 +0900

    Title: 신기한 타일 뒤집기, Time: 111ms, Memory: 26MB, Status: Wrong Answer - CodeTree

---
+++ 240507/신기한 타일 뒤집기/strange-flipping-tiles.py
@@ -0,0 +1,24 @@
+n = int(input())
+arr = [0 for _ in range(200000)]
+dm = []
+
+for _ in range(n):
+    a, b = input().split()
+    dm.append([int(a), b])
+
+start = 100000
+dest = 0
+
+for d in dm:
+    if d[1] == "R":
+        dest = start + d[0] - 1
+        for i in range(start, dest):
+            arr[i] = 1
+            start = dest
+    else:
+        dest = start - d[0] + 1
+        for i in range(start, dest, -1):
+            arr[i] = -1
+            start = dest
+
+print(arr.count(1), arr.count(-1))
