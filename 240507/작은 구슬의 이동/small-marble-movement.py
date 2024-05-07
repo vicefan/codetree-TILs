8822954abd2d9cc9396ff79b379bdd249ffe8b27
vicefan <131180965+vicefan@users.noreply.github.com>
2024-05-07 21:29:20 +0900

    Title: 작은 구슬의 이동, Time: 141ms, Memory: 24MB, Status: Passed - CodeTree

--- 240507/작은 구슬의 이동/small-marble-movement.py
+++ 240507/작은 구슬의 이동/small-marble-movement.py
@@ -1,25 +1,28 @@
-n, t = map(int, input().split())
+mapper = {
+    'R': 0,
+    'D': 1,
+    'U': 2,
+    'L': 3
+}
+
 dxs, dys = ([0, 1, -1, 0], 
             [1, 0, 0, -1])
 
-dr = {
-    'R': 0,
-    'U': 1,
-    'D': 2,
-    'L': 3
-}
+n, t = map(int, input().split())
+r, c, d = input().split()
+r, c = int(r), int(c)
+dir_num = mapper[d]
+
 
 def in_range(x, y):
     return 1 <= x <= n and 1 <= y <= n
 
-r, c, d = input().split()
-r, c = int(r), int(c)
-d_num = dr[d]
 
 for _ in range(t):
-    if in_range(r + dxs[d_num], c + dys[d_num]):
-        r, c = r + dxs[d_num], c + dys[d_num]
+    nr, nc = r + dxs[dir_num], c + dys[dir_num]
+    if in_range(nr, nc):
+        r, c = nr, nc
     else:
-        d_num = 3 - d_num
+        dir_num = 3 - dir_num
 
 print(r, c)
