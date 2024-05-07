d6d337a1e209ab2852bc43ffc38a8ebfc18c1727
vicefan <131180965+vicefan@users.noreply.github.com>
2024-05-07 19:37:45 +0900

    Title: 방향에 맞춰 이동, Time: 151ms, Memory: 24MB, Status: Passed - CodeTree

--- 240507/방향에 맞춰 이동/move-in-direction.py
+++ 240507/방향에 맞춰 이동/move-in-direction.py
@@ -1,18 +1,15 @@
 x, y = 0, 0
-dx, dy = ([1, 0, -1, 0], 
-          [0, -1, 0, 1])
+dx, dy = ([1, -1, 0, 0], 
+          [0, 0, -1, 1])
 
 N = int(input())
-dirt = ["E", "S", "W", "N"]
+dirt = ["E", "W", "S", "N"]
 
-arr = []
 for _ in range(N):
     dir_str, dist = input().split()
-    arr.append([dir_str, int(dist)])
-
-for elem in arr:
-    dir_num = dirt.index(elem[0])
-    nx, ny = x + dx[dir_num] * elem[1], y + dy[dir_num] * elem[1]
+    dist = int(dist)
+    dir_num = dirt.index(dir_str)
+    nx, ny = x + dx[dir_num] * dist, y + dy[dir_num] * dist
     x, y = nx, ny
 
 print(x, y)
