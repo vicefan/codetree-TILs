a9805d7365689182ce5a0ca75ac18b07a87e1e0e
vicefan <131180965+vicefan@users.noreply.github.com>
2024-05-09 18:53:57 +0900

    Title: 되돌아오기 2, Time: 107ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240509/되돌아오기 2/come-back-2.py
@@ -0,0 +1,17 @@
+#            R, D, L, U
+dxs, dys = ([0, 1, 0, -1], 
+            [1, 0, -1, 0])
+
+x, y, cnt, dir_num = 0, 0, 0, 0
+
+for elem in input():
+    if x == 0 and y == 0 and cnt != 0:
+        continue
+    else:
+        if elem == "R":
+            dir_num = (dir_num + 1) % 4
+        else:
+            x, y = x + dxs[dir_num], y + dys[dir_num]
+        cnt += 1
+
+print(cnt) if x == 0 and y == 0 else print(-1)
