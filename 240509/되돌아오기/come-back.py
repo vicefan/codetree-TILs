079a43d7b107fb3740d393540ec05e0791589374
vicefan <131180965+vicefan@users.noreply.github.com>
2024-05-09 15:26:27 +0900

    Title: 되돌아오기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240509/되돌아오기/come-back.py
@@ -0,0 +1,27 @@
+#            R, D, L, U
+dxs, dys = ([0, 1, 0, -1], 
+            [1, 0, -1, 0])
+
+drt = {
+    "N": 0,
+    "E": 1,
+    "S": 2,
+    "W": 3
+}
+
+x, y, cnt, n = 0, 0, 1, int(input())
+
+while cnt != n + 1:
+    if x == 0 and y == 0 and cnt != 1:
+        break
+    d, ni = input().split()
+    ni, dir_num, c = int(ni), drt[d], 0
+    for _ in range(ni):
+        if x == 0 and y == 0 and cnt != 1:
+            continue
+        else:
+            x, y = x + dxs[dir_num], y + dys[dir_num]
+            c += 1
+    cnt += c
+
+print(cnt - 1)
