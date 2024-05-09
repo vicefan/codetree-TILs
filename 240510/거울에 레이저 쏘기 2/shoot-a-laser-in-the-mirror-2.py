5ece88be796d0462ee2de8b5d94db9d9074a12df
vicefan <131180965+vicefan@users.noreply.github.com>
2024-05-10 02:52:29 +0900

    Title: 거울에 레이저 쏘기 2, Time: 104ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240510/거울에 레이저 쏘기 2/shoot-a-laser-in-the-mirror-2.py
@@ -0,0 +1,45 @@
+#            R, D, L, U
+dxs, dys = ([0, 1, 0, -1], 
+            [1, 0, -1, 0])
+
+n = int(input())
+
+arr = [
+    list(input())
+    for x in range(n)
+]
+
+k = int(input())
+k_ = k % n
+if 1 <= k <= n:
+    dir_num = 1
+    x, y = 0, k_ - 1
+elif n + 1 <= k <= 2*n:
+    dir_num = 2
+    x, y = k_ - 1, 2
+elif 2*n + 1 <= k <= 3*n:
+    dir_num = 3
+    x, y = 2, n - k_
+else:
+    dir_num = 0
+    x, y = 0, n - k_
+
+def in_range(x, y):
+    return 0 <= x < n and 0 <= y < n
+
+cnt = 0
+while in_range(x, y):
+    if arr[x][y] == "/":
+        if dir_num == 1 or dir_num == 3:
+            dir_num = (dir_num + 1) % 4
+        else:
+            dir_num = (dir_num - 1) % 4
+    else:
+        if dir_num == 1 or dir_num == 3:
+            dir_num = (dir_num - 1) % 4
+        else:
+            dir_num = (dir_num + 1) % 4
+    x, y = x + dxs[dir_num], y + dys[dir_num]
+    cnt += 1
+
+print(cnt)
